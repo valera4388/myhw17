dcf1e3fe1347709510109b6d4d6626cac51a9b73
mikhalev valera <mihalev.valera@mail.ru>
2024-10-26 19:05:27 +0300

    revert

--- src/test/java/ru/netology/rest/PostmanEchoTests.java
+++ src/test/java/ru/netology/rest/PostmanEchoTests.java
@@ -20,7 +20,7 @@ public class PostmanEchoTests {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("data"))
+                .body("data", equalTo("some data"))
         ;
     }
 }
