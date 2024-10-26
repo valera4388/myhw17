e4183d83ce6cdd4820746b9ab0f50ed9e507aaed
mikhalev valera <mihalev.valera@mail.ru>
2024-10-26 19:03:57 +0300

    crush test

--- src/test/java/ru/netology/rest/PostmanEchoTests.java
+++ src/test/java/ru/netology/rest/PostmanEchoTests.java
@@ -20,7 +20,7 @@ public class PostmanEchoTests {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some data"))
+                .body("data", equalTo("data"))
         ;
     }
 }
